22a323052e8ede090f94d1403865043ea93a85d7
KakaoTocs <kimjinu5555@gmail.com>
2018-04-26 20:13:21 +0900

    Weather update

---
+++ Weather/Weather/Country.swift
@@ -0,0 +1,13 @@
+//
+//  Country.swift
+//  Weather
+//
+//  Created by DGSW_TEACHER on 2018. 4. 26..
+//  Copyright © 2018년 KakaoTocs. All rights reserved.
+//
+
+struct Country: Codable {
+    let country: String
+    let imageName: String
+    let city: [String]
+}
---
+++ Weather/Weather/MainTableViewController.swift
@@ -0,0 +1,107 @@
+//
+//  MainTableViewController.swift
+//  Weather
+//
+//  Created by DGSW_TEACHER on 2018. 4. 26..
+//  Copyright © 2018년 KakaoTocs. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var countries: [Country] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "Country") else {
+            return
+        }
+        do {
+            self.countries = try jsonDecoder.decode([Country].self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.countries.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "countryCell", for: indexPath)
+
+        let country: Country = self.countries[indexPath.row]
+        
+        cell.textLabel?.text = country.country
+        cell.imageView?.image = UIImage(named: country.imageName)
+        cell.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
