b543d53e0a9169fb26cb0ffdec3ad30d42c234fd
KakaoTocs <kimjinu5555@gmail.com>
2018-05-12 20:48:04 +0900

    CommonCrypto
    use from MD5

---
+++ CommonCrypto/Source/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  CommonCrypto-iOS
+//
+//  Created by DGSW_TEACHER on 2018. 5. 12..
+//  Copyright © 2018년 Sam Soffes. All rights reserved.
+//
+
+import Foundation
+import CommonCrypto
