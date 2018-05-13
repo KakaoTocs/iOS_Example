49967e621de97923c3829ff7287c60020d674b05
KakaoTocs <kimjinu5555@gmail.com>
2018-05-13 20:41:23 +0900

    custom collectionViewCell Example
    커스텀셀 사용
    텍스트
    이미지

---
+++ FriendsCollection/FriendsCollection/FriendCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FriendCollectionViewCell.swift
+//  FriendsCollection
+//
+//  Created by DGSW_TEACHER on 2018. 5. 13..
+//  Copyright © 2018년 KakaoTocs. All rights reserved.
+//
+
+import UIKit
+
+class FriendCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var nameAgeLabel: UILabel!
+    @IBOutlet var adressLabel: UILabel!
+    @IBOutlet var faceImageView: UIImageView!
+    
+}
--- FriendsCollection/FriendsCollection/ViewController.swift
+++ FriendsCollection/FriendsCollection/ViewController.swift
@@ -8,24 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     var numberOfCell: Int = 10
     let cellIdentifier: String = "cell"
+    var friends: [Friend] = []
+    @IBOutlet weak var collectionView: UICollectionView!
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.numberOfCell
+        return self.friends.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellIdentifier, for: indexPath)
+        let cell: FriendCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellIdentifier, for: indexPath) as! FriendCollectionViewCell
+        
+        let friend: Friend = self.friends[indexPath.item]
+        
+        cell.faceImageView.image = UIImage(named: "Photo16.jpg")
+        cell.nameAgeLabel.text = friend.nameAndAge
+        cell.adressLabel.text = friend.fullAddress
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.numberOfCell += 1
+        collectionView.reloadData()
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "friends") else {
+            return
+        }
+        
+        do {
+            self.friends = try jsonDecoder.decode([Friend].self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        self.collectionView.reloadData()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
