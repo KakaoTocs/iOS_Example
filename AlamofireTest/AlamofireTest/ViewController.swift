6a71819b961c367e984eecad5da97c848167617a
KakaoTocs <kimjinu5555@gmail.com>
2018-05-16 21:05:11 +0900

    Alamofire 예제 수정

--- AlamofireTest/AlamofireTest/ViewController.swift
+++ AlamofireTest/AlamofireTest/ViewController.swift
@@ -11,28 +11,10 @@ import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        guard let url = URL(string: "10.80.161.231") else {
-            return
-        }
-        Alamofire.request(url, method: .get)
-//        .validate()
-        .responseJSON { response in
-            print(response)
-            guard response.result.isSuccess else {
-                print("Error")
-                return
-            }
-            
-//            guard let value = response.result.value as? [String: Any] else {
-//                print("Type Error")
-//            }
-            
-            print(response)
-        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,6 +22,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonAction(_ sender: Any) {
+        request("http://www.httpbin.org/get").responseJSON { response in
+            if let JSON = response.value {
+                var jsonObject = JSON as! [String: AnyObject]
+                let origin = jsonObject["origin"] as! String
+                let url = jsonObject["url"] as! String
+                
+                var temp = "JSON: \(jsonObject)"
+                temp.append("\nIP Address Origin: \(origin)")
+                temp.append("\nURL of Request: \(url)")
+                print("Hello, this is message 1")
+                
+                self.textView.text = temp
+            }
+        }
+        print("Hello, this is message 2")
+    }
+    
 }
 
