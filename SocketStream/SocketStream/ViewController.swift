9c909e5d27abb739970af42e0a2364aea36c2343
KakaoTocs <kimjinu5555@gmail.com>
2018-07-14 11:26:06 +0900

    Realm붙이기

--- SocketStream/SocketStream/ViewController.swift
+++ SocketStream/SocketStream/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func connectAction(_ sender: Any) {
-        socket.connectToServer(host: "www.phenology.info", port: 4109)
+        socket.connectToServer(host: "www.phenology.info", port: 5109)
 //        socket.connectToServer(host: "www.naver.com", port: 80)
     }
     
@@ -49,16 +49,16 @@ class ViewController: UIViewController {
         let ID: String = "sunge227"
         let idSize: String = "\((ID.characters.count) + 1)000"//String(bytes: toByteArray(((ID.characters.count) + 1) as Int), encoding: String.Encoding.utf8)!
         let separator: String = "\0"
-        let pwMD5: String = String.toMD5(PW)()
+        let pwMD5: String = PW //String.toMD5(PW)()
         let end: String = "0"//String(bytes: toByteArray(0 as Int), encoding: String.Encoding.utf8)!
         let android: [UInt8] = [3, 0, 0, 0, 9, 115, 117, 110, 103, 101, 50, 50, 55, 0, 57, 52, 53, 55, 100, 48, 55, 56, 57, 100, 57, 57, 56, 50, 54, 100, 102, 102, 51, 51, 97, 56, 56, 98, 53, 54, 48, 57, 101, 98, 0]
         let androidString: String = fromByteArray(android, String.self)
-        print(androidString)
+//        print(androidString)
         let query: String = temp + opCode + idSize + ID + separator + pwMD5 + end
         print(query)
 
         let queryData = query.data(using: String.Encoding.utf8, allowLossyConversion: true)
-        
+//        let queryData = androidString.data(using: String.Encoding.utf8, allowLossyConversion: true)
         let array = [UInt8](queryData!)
         print(array)
 
