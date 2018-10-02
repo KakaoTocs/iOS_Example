b5feaae7985b323a43ef63bf110f6f618bede30d
KakaoTocs <kimjinu5555@gmail.com>
2018-10-02 11:56:08 +0900

    UIImage Masking 예제 추가
    Sketch로 masking용 이미지 만들기 -> 적용

--- ImageMask/ImageMask/ViewController.swift
+++ ImageMask/ImageMask/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    let maskView  = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imageView.image = UIImage(named: "Image.jpg")
+        maskView.image = UIImage(named: "Mask.png")
+        imageView.mask = maskView
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        maskView.frame = imageView.bounds
+    }
 
 }
 
